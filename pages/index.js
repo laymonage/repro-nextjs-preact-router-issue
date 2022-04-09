4df785e9410130d5e3263a58765159e7cd6555f7
Sage Abdullah <me@laymonage.com>
2022-04-09 12:00:47 +0700

    Add button handler to demonstrate issue

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("do something");
+    router.replace("/other");
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -16,8 +22,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <button onClick={handleClick}>Click here to test the issue</button>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +66,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
